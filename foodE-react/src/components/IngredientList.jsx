da253fa750b0aff135d845362cba236490bc2967
Rhys Frazier <rhysrfrazier@gmail.com>
2023-11-01 15:13:31 -0400

    added IngredientList

--- foodE-react/src/components/IngredientList.jsx
+++ foodE-react/src/components/IngredientList.jsx
@@ -2,42 +2,56 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
 import { BASE_URL } from '../globals'
+import SearchBar from "./SearchBar"
 
 
 export default function IngredientList() {
 
+    const [searchTerm, setSearchTerm] = useState('')
+
     const [drinks, setDrinks] = useState([])
 
-    useEffect(()=>{
-        const getIngredients = async() => {
-            const response = await axios.get(`${BASE_URL}filter.php?i=Gin`)
-            setIngredients(response.data.drinks)
-            console.log(response)
-            setDrinks(response.data.drinks)
+    useEffect(() => {
+        if (searchTerm) {
+            const getIngredients = async () => {
+                const response = await axios.get(`${BASE_URL}filter.php?i=${searchTerm}`)
+                setDrinks(response.data.drinks)
+            }
+            getIngredients()
+        } else {
+            setDrinks([])
         }
-        getIngredients()
-    },[])
+    }, [searchTerm])
 
     let navigate = useNavigate()
 
-    const showIngredients = (key) => {
-        navigate(`${key}`)
+    const showIngredients = (idDrink) => {
+        navigate(`${idDrink}`)
     }
 
-    if (ingredients.length === 0) {
-        return <h2 className="Loading">Loading Please Wait...</h2>
-    } else {
-        return(
-            <div className="ingredients">
+    const handleSearch = (term) => {
+        setSearchTerm(term)
+    }
 
-                {
-                    ingredients.map((ingredient, key) => (
-                        <div key={ingredient.strDrink} onClick={()=>showIngredients(key)} className="card">
-                        <h3>{ingredient.strDrink}</h3>
+    return (
+        <div>
+            <SearchBar onSearch={handleSearch} />
+            {drinks.length === 0 ? (
+                <h2 className='Loading'>Please Enter an Ingredient to Search by</h2>
+            ) : (
+                <div className="drinks">
+
+                    {drinks.map((drink) => (
+                        <div key={drink.idDrink} onClick={() => showDrinkDetails(drink.idDrink)} className="card">
+
+                            <img src={drink.strDrinkThumb} alt={drink.strDrink} className="image-main" />
+
+                            <h3>{drink.strDrink}</h3>
                         </div>
-                    ))
-                }
-            </div>
-        )
-    }
+                    ))}
+                </div>
+            )}
+        </div>
+    )
+
 }
