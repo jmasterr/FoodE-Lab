a94432e797ba0cdd26522c77a8249695548b8d5d
Ryan-Fitzgerald-9 <rfitzgerald806@gmail.com>
2023-11-01 11:08:03 -0400

    list update

--- foodE-react/src/components/SearchBar.jsx
+++ foodE-react/src/components/SearchBar.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from "react"
 
-export default function SearchBar({onSearch}){
+export default function SearchBar({onSearch}) {
 
-    const [searchTerm, setSearchTerm] =useState('')
+    const [searchTerm, setSearchTerm] = useState('');
 
-    const handleChange = (e) => {
-        onSearch(searchTerm)
-    }
-    
-    return(
-        <div className="searchbar">
-            <input
-            type='text'
-            placeholder='Search drinks...'
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleSearch = () => {
+        onSearch(searchTerm);
+    };
+
+    return (
+        <div className="search-bar">
+        <input
+            type="text"
+            placeholder="Search drinks..."
             value={searchTerm}
-            onChange={handleChange} //why do we need this????
-            />
-            <button onClick={handleChange} >Search</button>
-
+            onChange={handleChange}
+        />
+        <button onClick={handleSearch}>Search</button>
         </div>
     )
 }
